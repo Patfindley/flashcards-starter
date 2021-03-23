beae0ac307fb003c87625d68582e1aefd9fe8d0a
Patrick Findley <patfindley@gmail.com>
2021-03-22 20:44:54 -0400

    First turn test

---
+++ test/Turn-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Turn', function() {
+  it('should be a function!' function() {
+      const turn = new Turn();
+      expect(Card).to.be.a('function');
+  })
+
+})
